237a49d6b224d48eb6614313b9dace5b6d1843ba
Tobias dpausp <dpausp@posteo.de>
2020-08-07 22:45:12 +0000

    Add HTTP basic auth for send endpoints

--- src/ekklesia_notify/main.py
+++ src/ekklesia_notify/main.py
@@ -1,16 +1,21 @@
 from random import randint
+import secrets
 from eliot import start_task
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from ekklesia_notify.lib.crypto import decode_recipient_info
 from ekklesia_notify.models import FreeformMessage, TemplatedMessage
 from ekklesia_notify import configure_logging
 from ekklesia_notify.transport.logging_dummy import LoggingDummyTransport
 from ekklesia_notify.transport.mail import MailTransport
 from ekklesia_notify.transport.matrix import MatrixTransport
+from ekklesia_notify.settings import clients
 
 configure_logging()
 
 app = FastAPI()
+
+security = HTTPBasic()
 
 TRANSPORTS = {
     'mail': MailTransport(),
@@ -19,13 +24,31 @@ TRANSPORTS = {
 }
 
 
+def identify_client(credentials: HTTPBasicCredentials = Depends(security)) -> str:
+    client_settings = clients.get(credentials.username, {})
+    expected_password = client_settings.get("password", "wrong username")
+    if client_settings:
+        password_correct = secrets.compare_digest(credentials.password, expected_password)
+    else:
+        password_correct = secrets.compare_digest("invalid", expected_password)
+
+    if not password_correct:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"})
+
+    return credentials.username
+
+
+
 @app.get('/')
 def api_info():
     return {'info': 'Ekklesia Notification service. Have a look at /docs to see how the messaging API can be used.'}
 
 
 @app.post('/templated_message')
-async def send_templated_message(msg: TemplatedMessage):
+async def send_templated_message(msg: TemplatedMessage, client_name: str = Depends(identify_client)):
 
     with start_task(task="send_templated_message"):
         transport = TRANSPORT()
@@ -37,7 +60,7 @@ async def send_templated_message(msg: TemplatedMessage):
 
 
 @app.post('/freeform_message')
-async def send_freeform_message(msg: FreeformMessage):
+async def send_freeform_message(msg: FreeformMessage, client_name: str = Depends(identify_client)):
 
     with start_task(task="send_freeform_message"):
 
